f68eb8cc66e8a6041aedf752572e19c9c42df595
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-04-19 08:54:51 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
 $(function () {
     console.log('loaded');
-    let DATABASE = {user:'', score:0, incorrect:0, correct:0, index:0, question:[], answers:[] };
-$('header').on("click", function(){
-    $('#thing1').animate({opacity:0, height:0, display:'none'},3000, function(){})
-    $('#thing2').hide(3000)
-});
-});
+    let DATABASE = {user:'', 
+    score:0, 
+    incorrect:0, 
+    correct:0, 
+    index:0, 
+    question:["Who is Forbe's Richest Man in 2018?", ""], 
+    answers:["Bill Gates", "Elon Musk", "Warren Buffet", "Jeff Bezos", 
+    ""] };
+// $('header').on("click", function(){
+//     $('#thing1').animate({opacity:0, height:0, display:'none'},3000, function(){})
+//     $('#thing2').hide(3000)
+// });
 
-$('#jsStartButton').on('click', function(){
+$('#jsStartButton').on('click', function() {
     console.log('clicked!')
-    $('#startPage').remove();
+    $('#startPage').hide();
     $('#questionPage').css('display', 'block');
-    $('form:nth-child(1)').css('display', 'block');
+    // $('form:nth-child(1)').css('display', 'block');
   });
 
-function createQuestion (correctAns, question, answeredQuest) {
-    return `
-    <section id="questionPage" role="main">
-    <h3 id="question">${question.quest}</h3>
-      
-      <form>
-        <fieldset>
-          <label>
-            <input class="answer" type="radio" name="option" required></input>
-            <span>Bill Gates</span>
-          </label>
-        
-          <label>
-            <input class="answer" type="radio" name="option" ></input>
-            <span>Elon Musk</span>
-          </label>
-        
-          <label>
-            <input class="answer" type="radio" name="option" ></input>
-            <span>Warren Buffet</span>
-          </label>
-        
-          <label>
-            <input class="answer" type="radio" name="option" ></input>
-            <span>Jeff Bezos</span>
-          </label>
-          </fieldset>
-          <button id="jsSubmitButton">Submit</button>
-      </form>
-      
-    <div id="progressBar">
-      <span id="questionNum">Question: 0/10 </span>
-      <span id="scoreBoard">Score: 0/10 </span>
-    </div>
-    </section> `
+function createQuestion () {
+    console.log(DATABASE);
+    $('#question').html(DATABASE.question[0]);
+    $('#ans1').html(DATABASE.answers[0]);
+    $('#ans2').html(DATABASE.answers[1]);
+    $('#ans3').html(DATABASE.answers[2]);
+    $('#ans4').html(DATABASE.answers[3]);
 }
+createQuestion();
+});
